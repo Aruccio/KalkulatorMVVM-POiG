7553101ed4f60ae11660bc898720a99fd52536f4
Aruccio <arleni@o2.pl>
2020-04-07 17:30:10 +0200

    1

---
+++ KalkulatorMVVM/KalkulatorMVVM/Model/Operations/Addition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorMVVM.Model.Operations
+{
+    class Addition : IOperation
+    {
+        public string Name { get; private set; } = "Addition";
+        public string Symbol { get; private set; } = "+";
+        public decimal Execute(decimal first, decimal sec)
+        {
+            return  first + sec;
+        }
+    }
+}
---
+++ KalkulatorMVVM/KalkulatorMVVM/Model/Operations/IOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorMVVM.Model.Operations
+{
+    interface IOperation
+    {
+        string Name { get; }
+        string Symbol { get; }
+        decimal Execute(decimal first, decimal second);
+    }
+}
